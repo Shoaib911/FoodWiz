9afb77451a3ca4993162d8374e4be87132f2de95
Shoaib <140606582+Shoaib911@users.noreply.github.com>
2024-05-19 17:38:25 +0500

    Controllers for crud operation

--- foodwiz/Backend/routes/workouts.js
+++ foodwiz/Backend/routes/workouts.js
@@ -1,37 +1,26 @@
 const express = require('express')
-const Workout=require('../models/workoutModel')
+const {
+    createWorkout,
+    getWorkout,
+    getWorkouts,
+    updateWorkout,
+    deleteWorkout
+}=require('../Controllers/workoutControllers')
 const router = express.Router()
 
 
 //Get all workouts
-router.get('/',(req, res) =>{
-    res.json({mssg:'Get all workouts'})
-})
+router.get('/',getWorkouts)
 
 //Get single workout
-router.get('/:id',(req,res)=>{
-    res.json({mssg:'Get single workout'})
-})
+router.get('/:id',getWorkout)
 
 //Post a new workout
-router.post('/',async(req,res)=>{
-    const {title,load,reps}=req.body
-    try{
-        const workout= await Workout.create({title,reps,load})
-        res.status(200).json(workout)
-    }catch{
-        res.status(400).json({error:err.message})
-    }
-    
-})
+router.post('/',createWorkout)
 
 //Delete workout
-router.delete('/:id',(req,res)=>{
-    res.json({mssg:'Delete workout'})
-})
+router.delete('/:id',deleteWorkout)
 //Update workout
-router.patch('/:id',(req,res)=>{
-    res.json({mssg:'Update workout'})
-})
+router.patch('/:id',updateWorkout)
 
 module.exports = router
