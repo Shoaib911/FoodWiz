fd2bbcf0b247efb1295c4dcd0e62d1c06a368150
Shoaib911 <shoaibaltaf200@gmail.com>
2024-06-02 02:18:20 +0500

    ai model and api

---
+++ foodwiz/AI/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, HTTPException
+from recommendation import recommend_recipe
+from schemas import RecipeRequest, RecipeResponse
+
+app = FastAPI()
+
+@app.post("/recommend_recipe/", response_model=RecipeResponse)
+async def recommend_recipe_endpoint(recipe: RecipeRequest):
+    try:
+        recommendations = recommend_recipe(
+            recipe.tags, recipe.ingredients, recipe.minutes, recipe.name, recipe.nutrition
+        )
+        return recommendations.to_dict(orient='records')
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="127.0.0.1", port=8000)
---
+++ foodwiz/AI/schemas.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+from typing import List, Optional
+
+class RecipeRequest(BaseModel):
+    tags: str
+    ingredients: str
+    minutes: int
+    name: str
+    nutrition: str
+
+class RecipeResponse(BaseModel):
+    name: str
+    ingredients: str
+    steps: str
+
