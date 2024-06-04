d7f9122dc8b2477eefab92f2d60b62fe4e815876
sufix <146112375+Sufixxx@users.noreply.github.com>
2024-06-04 19:57:39 +0500

    Ai updation, ht access

--- foodwiz/AI/main.py
+++ foodwiz/AI/main.py
@@ -1,20 +1,16 @@
 from typing import Any, Dict
 from fastapi import FastAPI, HTTPException
-from recommendation import recommend_recipe
-from schemas import RecipeRequest, RecipeResponse
+from recommendation import recommend_recipe  # Assuming this module is correctly implemented
+from schemas import RecipeRequest, RecipeResponse  # Assuming these schemas are defined
 
 app = FastAPI()
 
-@app.post("/recommend_recipe")
-async def recommendRecipe(recipe: RecipeRequest):
-    try:
-        print(recipe)
-        recommendations = recommend_recipe(
-            recipe.tags, recipe.ingredients, recipe.minutes, recipe.name, recipe.nutrition
-        )
-        return recommendations.to_dict(orient='records')
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.get("/")
+async def root():
+    print("Hello, Python")
+    return {"message": "Hello, Python"}
 
 if __name__ == "__main__":
     import uvicorn
